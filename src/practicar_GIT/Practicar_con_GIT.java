7dbb10a5cb4d95cf02bbefe6d611b809f9bb6b69
Scipio202 <mg_oscar@hotmail.com>
2024-03-19 12:13:12 +0100

    Creación de variable saludo

--- src/practicar_GIT/Practicar_con_GIT.java
+++ src/practicar_GIT/Practicar_con_GIT.java
@@ -4,7 +4,9 @@ public class Practicar_con_GIT {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola git.");
+		String saludo = "Hola git.";
+		
+		System.out.println(saludo);
 
 	}
 
