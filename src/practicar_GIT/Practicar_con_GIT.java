629c287e342e46d3a077c714cb220a36dd6845bb
Scipio202 <130449943+Scipio202@users.noreply.github.com>
2024-03-21 10:51:08 +0100

    Update Practicar_con_GIT.java
    Agregado de un bucle for.

--- src/practicar_GIT/Practicar_con_GIT.java
+++ src/practicar_GIT/Practicar_con_GIT.java
@@ -6,9 +6,10 @@ public class Practicar_con_GIT {
 		
 		String saludo = "Hola git.";
 		int numero = 7;
-		
-		System.out.println(saludo + numero);
 
+		for(int i=0; i<4; i++){
+		System.out.println(saludo + numero);
+		}
 	}
 
 }
